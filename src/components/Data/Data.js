2edaa228850257004f53a2c051754ef4f4458937
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-27 12:47:51 -0700

    Added states

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -1,1 +1,23 @@
- 
+import React, { useState } from "react";
+import API from "../../utils/API";
+import "./Data.css";
+
+export default function Data() {
+  const [users, setUsers] = useState([]);
+  const [order, setOrder] = useState("ascend");
+  const [userFilter, setUserFilter] = useState([]);
+  const [headings, setHeadings] = useState([
+    {
+      name: "Name"
+    },
+    {
+      name: "Phone"
+    },
+    {
+      name: "Email"
+    },
+    {
+      name: "DOB"
+    }
+  ]);
+}
