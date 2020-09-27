0f9e4657945ad6a9ccc844bbce191bee1f311628
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-27 13:54:20 -0700

    added sorting functionality

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -20,4 +20,45 @@ export default function Data() {
       name: "DOB"
     }
   ]);
+
+  const handleSorting = heading => {
+    if (order === "descend") {
+      setOrder("ascend");
+    } else {
+      setOrder("descend");
+    }
+
+    const comparison = (a, b) => {
+      if (order === "ascend") {
+        if (a[heading] === undefined) {
+          // Place heading last
+          return 1;
+        } else if (b[heading] === undefined) {
+          // place heading last
+          return -1;
+        } else if (heading === "name") {
+          // If the heading has a name then make sort in alphabetic
+          return a[heading].first.localeCompare(b[heading].first);
+        } else {
+          // Compare the headings values in descending order.
+          return b[heading] - a[heading];
+        }
+      } else {
+        if (a[heading] === undefined) {
+          return 1;
+        } else if (b[heading] === undefined) {
+          return -1;
+        } else if (heading === "name") {
+          return b[heading].first.localeCompare(a[heading].first);
+        } else {
+          return b[heading] - a[heading];
+        }
+      }
+    };
+
+    const userSort = userFilter.sort(comparison);
+
+    setUserFilter(userSort);
+  };
+ 
 }
