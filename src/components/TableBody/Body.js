c8c874e979d23b386b380be17e7ae4184b4eca7c
Cyrus Jose <cyrus.jose@outlook.com>
2020-09-27 23:03:16 -0700

    Testing sort and style

--- src/components/TableBody/Body.js
+++ src/components/TableBody/Body.js
@@ -24,23 +24,23 @@ export default function Body() {
               <tr key={login.uuid} className = "rowHover">
                 <td data-th="Image" className="employee">
                   <img
-                    src={picture.large}
+                    src={picture.medium}
                     alt={"profile image for " + name.first + " " + name.last}
                     className="img-responsive empImage"
                   />
                 </td>
-                <td data-th="Name" className="info pt-5 name-cell text-center">
+                <td data-th="Name" className="info pt-5 name-cell">
                   {name.first} {name.last}
                 </td>
-                <td data-th="Phone" className="info pt-5 text-center">
+                <td data-th="Phone" className="info pt-5">
                   {phone}
                 </td>
-                <td data-th="Email" className="info pt-5 text-center">
+                <td data-th="Email" className="info pt-5">
                   <a href={"mailto:" + email} target="__blank">
                     {email}
                   </a>
                 </td>
-                <td data-th="DOB" className="info pt-5 text-center">
+                <td data-th="DOB" className="info pt-5">
                   {dateFormatting(dob.date)}
                 </td>
               </tr>
